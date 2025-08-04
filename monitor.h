a4d08af6aed3a3d3216be3df80abade06645b7d3
shreyas7661 <shreyas1ga17ec135@gmail.com>
2025-08-04 15:58:16 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,22 @@
 #pragma once
+#include <string>
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+struct VitalLimits {
+  float lower;
+  float upper;
+};
+
+enum VitalStatus {
+  VITAL_OK,
+  VITAL_LOW,
+  VITAL_HIGH
+};
+
+// Pure function to evaluate a vital
+VitalStatus evaluateVital(float value, const VitalLimits& limits);
+
+// Public function
+bool vitalsOk(float temperature, float pulseRate, float spo2);
+
+// For testing individual vitals or expansion
+bool checkVital(const std::string& name, float value, const VitalLimits& limits);
